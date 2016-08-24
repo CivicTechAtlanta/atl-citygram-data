9313b1f3e0e237734083824ec3a60a8e23a927cd
Vincent Nguyen <vincentcgi@gmail.com>
2016-08-23 23:11:09 -0400

    Write XML feed from atlanta.net to CSV

--- atlcity_events/atlanta_net_scrapper.rb
+++ atlcity_events/atlanta_net_scrapper.rb
@@ -1,25 +1,26 @@
 require 'csv'
-require 'date'
-require 'nokogiri'
 require 'json'
 require 'net/http'
 require "crack"
+require "csv"
 
 class CityEvents
 
 	def initialize
-		puts "initializing..."
+		# Get data from feed, parse XML, and convert to JSON
 		data = Net::HTTP.get_response(URI.parse('http://feeds.feedburner.com/ThingsToDoInAtlanta?format=xml')).body
 		xml = Crack::XML.parse(data)
-		puts xml.to_json
-		# JSON.pretty_generate(Hash.from_xml(s).to_json)
-		
-    	# @xml_events_feed = 'http://feeds.feedburner.com/ThingsToDoInAtlanta?format=xml'
+		@json_data = JSON.parse(xml.to_json)
   	end
 
 	def get_events
-		
-		puts "Getting events"
+		# Write to CSV file
+		CSV.open("./file_atlcity_events.csv", "wb") do |csv|
+			csv << ["Description", "PubDate", "Link"]
+			@json_data["rss"]["channel"]["item"].each do |event|
+				csv << [event["title"], event["pubDate"], event["link"]]
+			end
+		end
 	end
 
 end
